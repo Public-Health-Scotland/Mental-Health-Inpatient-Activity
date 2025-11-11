a72e58e793554b68a813e901159d49cc20f3dca3
alex-bruce <alex.bruce@phs.scot>
2025-11-11 18:30:24 +0000

    Routine updates and adding LD link

--- modules/ui_00_introduction.R
+++ modules/ui_00_introduction.R
@@ -65,6 +65,14 @@ tabPanel(
     ),
     tags$li(
       tags$b(
+        actionLink("link_to_learning_disabilities_tab", "Learning Disabilities")
+      ),
+      icon("school"),
+      " - shows activity in the Learning Disability specialty."
+    ),
+    
+    tags$li(
+      tags$b(
         actionLink("link_to_table_tab", "Table")
       ),
       icon("table"),
@@ -82,67 +90,69 @@ tabPanel(
           (SMR04) datasets. SMR04 includes all inpatients and day cases
           admitted to and discharged from psychiatric specialties. SMR01
           includes all inpatients and day cases discharged from non-obstetric
-          and non-psychiatric specialties. For a complete list of
-          specialties, open the ",
-      tags$a(
-        href = "https://www.ndc.scot.nhs.uk/docs/2020-04-23%20Specialty-Codes-and-Values.xlsx",
-        "Specialty Codes and Descriptions document"
-      ),
-      " and consult the columns SMR01 and SMR04. Please note that
-          this data release excludes activity in the Learning Disability
-          specialty. Data on patients being treated in Long Stay Geriatric 
-          specialties is also not represented in this data release as it is 
-          recorded in a separate dataset. Additionally, SMR01 has been 
-          restricted to diagnoses of 'Mental and behavioural disorders' only, 
-          which were identified using codes F00-F99 from the International 
-          Classification of Diseases, Tenth Revision."
-    ),
+          and non-psychiatric specialties. Click the following links for a 
+      complete list of ",
+      tags$a("SMR01 Specialty Codes",
+        href = "https://publichealthscotland.scot/resources-and-tools/health-intelligence-and-data-management/national-data-catalogue/data-dictionary/search-the-data-dictionary/smr01-significant-facilities-and-codes/",
+        target = "_blank"
+        ),
+      " and ",
+      tags$a("SMR04 Specialty Codes.",
+        href = "https://publichealthscotland.scot/resources-and-tools/health-intelligence-and-data-management/national-data-catalogue/data-dictionary/search-the-data-dictionary/smr04-significant-facilities-and-codes/",
+        target = "_blank"
+      ))),
+  tags$ul(
+    tags$li(
+      "Please note that this data release excludes activity in the Learning Disability 
+      specialty except on the Learning Disabilities tab.")),
+  tags$ul(
+    tags$li(
+      "Data on patients being treated in Long Stay Geriatric specialties (SMR01_1E) 
+      and data from maternity inpatient and day cases (SMR02) are not represented in this data release.")),
+  tags$ul(
+    tags$li(
+      "Additionally, SMR01 has been restricted to diagnoses of 'Mental and behavioural disorders' only, 
+      which were identified using codes F00-F99 from the International 
+      Classification of Diseases, Tenth Revision.")),
+  tags$ul(
     tags$li(
       "SMR01 and SMR04 data completeness varies from year to year.
           As a result, data is provisional and subject to change. For
           more information, visit the ",
-      tags$a(
-        href = "https://beta.isdscotland.org/products-and-services/data-management-hospital-activity/smr-completeness/",
-        "SMR Completeness"
-      ),
+      tags$a("SMR Completeness",
+        href = "https://publichealthscotland.scot/resources-and-tools/health-intelligence-and-data-management/data-management-in-secondary-care-hospital-activity/scottish-morbidity-records-smr/completeness/",
+        target = "_blank"),
       " webpage, which contains an Excel file with completeness estimates 
-          for all SMR datasets. For this release, data completeness for the State 
-          Hospital (SMR04) was slightly lower than other health boards at the
-          time of data extraction, at 87%. This incompleteness only affects
-          Quarter 4 (38%) of the financial year. This was due to ongoing 
-          staffing issues at the State Hospital. A judgement has been made to
-          proceed with the publication as the lower data completeness for
-          this health board has only a very minor effect on the overall data
-          quality for this release and is very unlikely to affect the patterns
-          of activity seen or conclusions drawn at a national level. Data 
-          presented for the year 2022/23 is 99% complete overall, well above 
-          the 90% PHS standard for recording of data."
-    ),
+        for all SMR datasets. For this release, data for the State Hospital (SMR04) 
+        was unavailable for the year 2023/24 due to issues with their systems. 
+        A judgement has been made to 
+        proceed with the publication as the lower data completeness for
+        this health board has only a very minor effect on the overall data
+        quality for this release and is very unlikely to affect the patterns
+        of activity seen or conclusions drawn at a national level. Data 
+        presented for the year 2023/24 is 98% complete overall, well above 
+        the 90% PHS standard for recording of data."
+    )),
+  tags$ul(
     tags$li(
       "Statistical disclosure control has been applied to protect patient 
           confidentiality by rounding values to the nearest 5. As a result, the 
           figures presented in this explorer may not be additive and may differ 
           from previous sources of information. For more information, 
           please refer to the ",
-      tags$a(
+      tags$a("PHS Statistical Disclosure Control Protocol.",
         href = "https://publichealthscotland.scot/publications/public-health-scotland-statistical-disclosure-protocol/",
-        "PHS Statistical Disclosure Control Protocol."
-      ),
-      ""
-    )
-  ),
+        target = "_blank"
+      )
+    )),
   
   # Provide a download button for the glossary.
   p("To help you understand the information visualised in the
         explorer, we have created a glossary of commonly used terms in
         mental health care. Click the button below to download the glossary:"),
-  downloadButton(outputId = "download_glossary_one",
+  downloadButton(outputId = "download_glossary_00",
                  label = "Download glossary",
-                 class = "glossaryone"),
-  tags$head(
-    tags$style(".glossaryone { background-color: #0072B2; }
-                   .glossaryone { color: #FFFFFF; }")
-  ),
+                 class = "glossaryButton"),
   
   # Provide contact details for the team.
   p(br(),
